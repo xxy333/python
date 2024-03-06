29e145331dc34ab8c90987599dbe8d0ee3b2a469
xxy333 <45463605+xxy333@users.noreply.github.com>
2024-03-07 00:02:48 +0100

    API project

--- APIs/working.py
+++ APIs/working.py
@@ -1,20 +1,38 @@
-from fastapi import FastAPI, Path
+from typing import Annotated, Optional
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-inventory = {
-    1: {
-        "name": "Milk",
-        "price": 3.99,
-        "brand": "Regular"
-    }
-}
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+
+inventory = {}
 
 @app.get("/get-item/{item_id}")
-def get_item(item_id: int = Path(None, description="The ID of the item you would like to view")):
+async def get_item(item_id: int):
     return inventory[item_id]
 
-#@app.post("/post-items/{item_id}")
-#def post_item(item_id: int):
-#    return inventory[item_id]
+
+
+
+@app.get("/get-by-name")
+async def get_item(name: str = Query(None, title="Name", description="Name of item.")):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+        return {"Data": "Not found"}
+    
+
+@app.post("/create-item/{item_id}")
+async def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error" : "Item ID already exists"}
+    
+    inventory[item_id] = item
+    return inventory[item_id]
