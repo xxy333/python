17894a066f877928daa7a5f2e4773b9214015ed8
xxy333 <45463605+xxy333@users.noreply.github.com>
2024-10-31 18:22:53 +0100

    feat: playing codewars

--- third_one/codewars/codewars.py
+++ third_one/codewars/codewars.py
@@ -1,8 +1,20 @@
 num_list = [1, 10, 12, 4, 3, 2, 3, 21, 30, 5]
 def buble_sorter(nums):
-    for value, index in enumerate(nums):
-        print(value)
+    for n in range(len(nums) -1, 0, -1):
+        for i in range(n):
+            if nums[i] > nums[i + 1]:
+                swapped = True
+                nums[i], nums[i + 1] = nums[i + 1], nums
+
+
+
+
+
+
+
 buble_sorter(num_list)
+print(num_list)
+
 
 
 
