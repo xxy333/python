4a2662608e185eef91a34bee46b9ebf64b942c5c
xxy333 <45463605+xxy333@users.noreply.github.com>
2024-10-31 19:24:03 +0100

    Add API.py file

--- APIs.py
+++ APIs.py
@@ -1,15 +1,30 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
-fibo_list = [1, 2, 3, 5, 8, 13]
+app = Flask(__name__)
 
-index_to_remove = -1
+db_server = MongoClient("mongodb://localhost:27017/")
+db = db_server["mydatabase"]
 
-while len(fibo_list) >= 3:
-    if fibo_list[-1] == fibo_list[-2] + fibo_list[-3]:
-        del fibo_list[index_to_remove]
-    else:
-        print("Nejedná se o fibonaciho")
-        break
-else:
-    if len(fibo_list) < 3:
-        print("Přílíš málo prvků k ověřne")
+
+@app.route("/send_new_data", methods = ["POST"])
+def send_data():
+    data = request.json
+    db_scheme = db["anothercollection"]
+    data_recieve = db_scheme.insert_one(data)
+    return jsonify({"message" : "Data succesefully send!", "id" : str(data_recieve.inserted_id)}), 201
+
+@app.route("/data_callback", methods = ["GET"])
+def return_data():
+    
+    return ("Ha, provolal jsi mě!")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
+
+
+    
+
+
 
