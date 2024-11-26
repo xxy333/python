4b48a09a202f9c92df9735cb0a132ceef4fb3f4b
xxy333 <45463605+xxy333@users.noreply.github.com>
2024-11-26 20:30:05 +0100

    feat: pushing new react code

---
+++ visual_code/react/frontend/src/ContactList.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+
+const ContactList = ({contacts}) => {
+    return <div>
+
+        <h2>Contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact)=> (
+                    <tr key={contact.id}>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                        <td>
+                            <button>Update</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                )
+                )}
+            </tbody>
+
+        </table>
+    </div>
+
+
+}
+
+export default ContactList
